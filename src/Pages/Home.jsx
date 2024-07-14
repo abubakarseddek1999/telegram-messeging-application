11af9a874bb6932b496b52dd21cd6632b911d5c7
abubakarseddek1999 <abubakarseddek1999@gmail.com>
2024-07-14 10:39:05 +0600

    basic setup project

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,17 @@
+
+const Home = () => {
+    return (
+        <div className=""
+        style={{ 
+            backgroundImage: 'url(https://i.postimg.cc/DyW4Ph20/10-CAP-US-Header-Key-Features-of-Project-Management-Software-and-Top-Rated-Products-For-Each-US-1200.png)', 
+            backgroundSize: 'cover', 
+            backgroundPosition: 'center', 
+            height: '100vh' 
+        }}
+        >
+            <p className="text-white text-4xl md:text-6xl font-bold pt-10 flex justify-center items-center">You can assign your task there</p>
+        </div>
+    );
+};
+
+export default Home;
